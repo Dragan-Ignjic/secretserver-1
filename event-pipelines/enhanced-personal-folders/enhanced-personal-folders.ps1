93057193aa921491daa75477cb49c7651b8e9edf
Rob Jagger <116566+jagger@users.noreply.github.com>
2023-04-06 09:12:47 -0400

    Update enhanced-personal-folders.ps1
    additional error  checking

--- event-pipelines/enhanced-personal-folders/enhanced-personal-folders.ps1
+++ event-pipelines/enhanced-personal-folders/enhanced-personal-folders.ps1
@@ -1,19 +1,27 @@
 if ($args.count -gt 4) {[string]$APIUsername = $args[4],$args[0] -join "\"}else{[string]$APIUsername = $args[0]}
+
 [securestring]$APIPassword =  ConvertTo-SecureString $args[1] -AsPlainText -Force
 $Username = $args[2]
 $FolderName = $args[3]
 
-$SecretServerURL = "https://server/url"
-$PFolderBaseID = 0
-$FolderOwnerUsers = @("Manager1","Manager2")
-$FolderOwnerGroups =  @("ManagementGroup")
-$AllowedTemplateIDs = @()
+### User Configuration Items ###
 
-#$logFile = "c:\temp\ss-EnhancedPersonalFolders.log"
-#(get-date).ToString() + ":`t" + $args[0,2,3] | Add-Content -path $logFile
+$SecretServerURL = "https://server/url"         #Your Secret Server URL   
+$PFolderBaseID = 0                              #FolderID for Base Folder
+$FolderOwnerUsers = @("Manager1","Manager2")    #Usernames as shown on secret server users
+$FolderOwnerGroups =  @("ManagementGroup")      #Groupnames as shown on secret server groups
+$AllowedTemplateIDs = @()                       #comma separated list of alllowed template ids
+#$logFile = "c:\temp\ss-EnhancedPersonalFolders.log"  #uncomment this line to enable logging to specified file
 
+### End User Config Items ### 
+$reqPermissions = "Administer Folders","View Folders","View Groups","View Roles","View Secret Policy","View Secret Templates","View Users"
+
+if ($null -ne $logfile){(get-date).ToString() + ":`t" + $args[0,2,3] | Add-Content -path $logFile}
 try {
     $Session = New-TssSession -SecretServer $SecretServerURL -Credential (New-Object System.Management.Automation.PSCredential ($APIUsername, $APIPassword))
+    if ($null -ne $logfile){(get-date).ToString() + ":`t" + "Connected to " + $session.secretserver | Add-Content -path $logFile}
+    [array]$missingPermissions = (Compare-Object -ReferenceObject (Show-TssCurrentUser -TssSession $session).Permissions.name -DifferenceObject $reqPermissions | Where-Object {$_.sideindicator -eq "=>"}).inputobject
+    if ($missingPermissions.Count) { throw "API Account missing Role Permisions: " + ($missingPermissions -join "/")}
     $existingFolder = Search-TssFolder -TssSession $Session -ParentFolderId $PFolderBaseID -SearchText $FolderName
     if ($null -eq $existingFolder) {
         $newFolder = New-TssFolder -TssSession $Session -FolderName $FolderName -ParentFolderId $PFolderBaseID -InheritPermissions:$false
